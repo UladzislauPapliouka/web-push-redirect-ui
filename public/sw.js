645ecc505a62518b919f5c413546e2f644fef864
UladzislauPapliouka <uladzislau.papliouka@gmail.com>
2025-01-21 17:55:22 +0300

    update public folder

---
+++ public/sw.js
@@ -0,0 +1,27 @@
+importScripts('firebase-messaging-sw.js')
+
+async function getClientList() {
+    return self.clients.claim().then(() =>
+        self.clients.matchAll({
+            type: 'window'
+        })
+    );
+}
+
+self.addEventListener('push', function(event) {
+    if (event.data) {
+        console.log('This push event has data: ', event.data.json());
+    } else {
+        console.log('This push event has no data.');
+    }
+});
+self.notificationclick =null
+
+self.addEventListener('notificationclick', async function(event) {
+   const [client] = await getClientList()
+    console.log('Notification clicked: ', event.notification.data.deeplink);
+    console.log('Window',this)
+    console.log("Clients", client)
+    // client.location.fre
+    client.navigate(event.notification.data.deeplink)
+})

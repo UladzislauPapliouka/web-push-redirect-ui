b98d818e7776c990dc57ae1d0b10b7cf4d4e4545
UladzislauPapliouka <uladzislau.papliouka@gmail.com>
2025-01-22 11:38:07 +0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
 import {Typography} from "@alfalab/core-components/typography";
 import {List} from "@alfalab/core-components/list";
-import { IoShareOutline } from "react-icons/io5";
-import { CgAddR } from "react-icons/cg";
+// import { IoShareOutline } from "react-icons/io5";
+// import { CgAddR } from "react-icons/cg";
 import {Button} from "@alfalab/core-components/button";
 // import {SystemMessage} from "@alfalab/core-components/system-message";
 // import {SuperEllipse} from "@alfalab/core-components/icon-view/components";
@@ -122,29 +122,29 @@ function App() {
   //     </div>
   // }
   // @ts-ignore
-  const isPWA = window.navigator.standalone
-  if (!isPWA || !window.matchMedia('(display-mode: standalone)').matches) {
-      return <div
-          style={{
-              boxSizing: 'border-box',
-              display:"flex",
-              flexDirection:"column",
-              alignItems:"center",
-              justifyContent:"space-between",
-              height:'100dvh',
-              padding:'10px'
-          }}>
-          <Typography.Title tag={"h1"} weight={"bold"} style={{textAlign: 'center'}}>Подключение web-push
-              уведомлений</Typography.Title>
-         <div> <Typography.Text style={{textAlign: 'center'}}>Инструкция:</Typography.Text>
-             <List tag={'ol'}>
-                 <List.Item>Нажать на кнопку поделиться <IoShareOutline/></List.Item>
-                 <List.Item>Нажать на кнопку Добавить на экран домой <CgAddR/></List.Item>
-                 <List.Item>Нажать на кнопку Добавить</List.Item>
-                 <List.Item>Перейти в появившееся приложение на экране домой</List.Item>
-             </List></div>
-      </div>
-  }
+  // const isPWA = window.navigator.standalone
+  // if (!isPWA || !window.matchMedia('(display-mode: standalone)').matches) {
+  //     return <div
+  //         style={{
+  //             boxSizing: 'border-box',
+  //             display:"flex",
+  //             flexDirection:"column",
+  //             alignItems:"center",
+  //             justifyContent:"space-between",
+  //             height:'100dvh',
+  //             padding:'10px'
+  //         }}>
+  //         <Typography.Title tag={"h1"} weight={"bold"} style={{textAlign: 'center'}}>Подключение web-push
+  //             уведомлений</Typography.Title>
+  //        <div> <Typography.Text style={{textAlign: 'center'}}>Инструкция:</Typography.Text>
+  //            <List tag={'ol'}>
+  //                <List.Item>Нажать на кнопку поделиться <IoShareOutline/></List.Item>
+  //                <List.Item>Нажать на кнопку Добавить на экран домой <CgAddR/></List.Item>
+  //                <List.Item>Нажать на кнопку Добавить</List.Item>
+  //                <List.Item>Перейти в появившееся приложение на экране домой</List.Item>
+  //            </List></div>
+  //     </div>
+  // }
   return (
       <div
           style={{

58d819a0e7a412ca8c0e5faa645cda12e1f30099
UladzislauPapliouka <uladzislau.papliouka@gmail.com>
2025-01-22 20:56:55 +0300

    Updates

--- sw.js
+++ sw.js
@@ -25,7 +25,17 @@ self.addEventListener('notificationclick', async function(event) {
     console.log('Notification clicked: ', event.notification.data.deeplink);
     console.log('Window',this)
     console.log("Clients", client)
-    self.clients.openWindow(event.notification.data.deeplink)
+
+    var promise = new Promise(function(resolve) {
+        setTimeout(resolve, 3000);
+    }).then(function() {
+        // return the promise returned by openWindow, just in case.
+        // Opening any origin only works in Chrome 43+.
+        return self.clients.openWindow(event.notification.data.deeplink)
+    });
+
+    // Now wait for the promise to keep the permission alive.
+    event.waitUntil(promise);
     // client.location.fre
     // waitUntil(()=>{
     //     client.navigate(event.notification.data.deeplink)
